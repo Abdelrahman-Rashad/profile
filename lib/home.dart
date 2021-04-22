96e989ca3899e9c6afe574e38392b87a8546079f
Abdelrahman Rashad <abdelrahman.a.rashad@gmail.com>
2021-04-23 00:49:36 +0200

    final

--- lib/home.dart
+++ lib/home.dart
@@ -11,16 +11,61 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Profile'),
-        leading: Icon(Icons.person),
+        leading: Icon(Icons.person_pin),
         backgroundColor: Colors.black54,
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Container(
-              color: Colors.black45,
-            )
-          ],
+      body: Container(
+        color: Colors.black87,
+        child: Center(
+          child: Column(
+            children: [
+              Container(
+                height: 200,
+                width: 200,
+                color: Colors.brown[200],
+              ),
+              Text(
+                'Name : ....',
+                style: TextStyle(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              Container(
+                color: Colors.blueGrey,
+                height: 15.0,
+              ),
+              Text(
+                'Age : ....',
+                style: TextStyle(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              Container(
+                color: Colors.blueGrey,
+                height: 15.0,
+              ),
+              Text(
+                'Education : ....',
+                style: TextStyle(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              Container(
+                color: Colors.blueGrey,
+                height: 15.0,
+              ),
+              Text(
+                'Address : ....',
+                style: TextStyle(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+            ],
+          ),
         ),
       ),
     );
